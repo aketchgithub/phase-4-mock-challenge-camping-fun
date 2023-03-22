16d623ee4c1e3b2b7791f8f34cdb2e771e2a8784
Victor Aketch <vickeryace@gmail.com>
2023-03-22 09:39:59 +0300

    Working on the fun activity api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :campers, only: [:index]
+  resources :activities
+  resources :signups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
